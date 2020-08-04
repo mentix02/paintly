03ca50af548d30d582e3af616106567b057d5d54
mentix02 <manan.yadav02@gmail.com>
2020-08-04 14:37:57 +0530

    Added method for faking image models.

--- shop/factory.py
+++ shop/factory.py
@@ -1,9 +1,13 @@
+import os
+import random
 from typing import Tuple
-from random import choice
 
 from faker import Faker
 
-from shop.models import Painting
+from django.conf import settings
+from django.core.files import File
+
+from shop.models import Painting, Image
 
 fake = Faker()
 dimensions: Tuple[Tuple[int, int], Tuple[int, int]] = (
@@ -12,11 +16,24 @@ dimensions: Tuple[Tuple[int, int], Tuple[int, int]] = (
 )
 
 
-def fake_painting() -> Painting:
-    height, width = choice(dimensions)
-    return Painting.objects.create(
+def fake_painting(images: int = 0) -> Painting:
+    height, width = random.choice(dimensions)
+    painting = Painting.objects.create(
         height=height,
         width=width,
         description=fake.text(150),
         name=' '.join(fake.words()),
     )
+    for _ in range(images):
+        fake_image(painting.id)
+    return painting
+
+
+def fake_image(painting_id: int) -> Image:
+    with open(
+        os.path.join(settings.BASE_DIR, 'media/static/dancing-girl.jpg'), 'br'
+    ) as file:
+        image = Image.objects.create(
+            file=File(file), caption=fake.text(15), painting_id=painting_id
+        )
+    return image
