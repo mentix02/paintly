8546ff57942b0d284e5d303cdfb541235dbb32b7
mentix02 <manan.yadav02@gmail.com>
2020-08-06 12:34:18 +0530

    Added check for testing databases.

--- buyer/tasks.py
+++ buyer/tasks.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+from django.db import connection
 from django.conf import settings
 from django.core.mail import send_mail
 from django.utils.html import strip_tags
@@ -10,15 +11,16 @@ from celery import shared_task
 
 def send_email(subject: str, template: str, email: str, link: str) -> None:
 
-    html_content = render_to_string(template, {'link': link})
-    text_content = strip_tags(html_content)
-    send_mail(
-        subject,
-        text_content,
-        settings.EMAIL_HOST_USER,
-        [email],
-        html_message=html_content,
-    )
+    if not connection.settings_dict['NAME'].rsplit('/')[-1].startswith('test_'):
+        html_content = render_to_string(template, {'link': link})
+        text_content = strip_tags(html_content)
+        send_mail(
+            subject,
+            text_content,
+            settings.EMAIL_HOST_USER,
+            [email],
+            html_message=html_content,
+        )
 
 
 @shared_task
